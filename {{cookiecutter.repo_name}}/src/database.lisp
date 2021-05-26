7fe8f19e1174f6bf7c7925eae8511d69add14f15
vindarel <vindarel@mailz.org>
2021-05-27 00:51:34 +0200

    database.lisp: about migrations

---
+++ {{cookiecutter.repo_name}}/src/database.lisp
@@ -0,0 +1,38 @@
+(in-package :{{ cookiecutter.project_name}}/models)
+;;;
+;;; DB connection, migrations.
+;;;
+
+(defparameter *tables* '(product)
+  "List of the DB tables that need to be checked for migrations.")
+
+(defun connect (&optional (db-name *db-name*))
+  "Connect to the DB."
+  ;; *db* could be mito:*connection*
+  (log:debug "connecting to ~a~&" *db-name*)
+  (setf *db* (mito:connect-toplevel :sqlite3 :database-name db-name)))
+
+(defun ensure-tables-exist ()
+  "Run SQL to create the missing tables."
+  (unless mito::*connection*
+    (connect))
+  (mapcar #'mito:ensure-table-exists *tables*))
+
+(defun migrate-all ()
+  "Migrate the tables after we changed the class definition."
+  (mapcar #'mito:migrate-table *tables*))
+
+;; (defun bootstrap-base-roles ()
+;;   (define-role :visitor ())
+;;   (define-role :editor (:vendor))
+;;   (define-role :admin (:editor))
+;;   t)
+
+;;
+;; Entry points
+;;
+(defun init-db ()
+  "Connect to the DB, run the required migrations and define a couple base user roles."
+  (ensure-tables-exist)
+  ;;(bootstrap-base-roles)
+  )
